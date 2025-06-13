d11bc5e9e4e32b32aee1662158343fa59339571b
PBLGroupG <pblgroupg994@gmail.com>
2025-06-13 17:27:09 +0900

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning className="bg-white dark:bg-gray-900">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <base href="PBLGroupG.github.io" />
       </head>
-      <body className="text-gray-800 dark:text-gray-200 font-sans transition-colors duration-300 min-h-screen">
+      <body className="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 font-sans transition-colors duration-300 min-h-screen">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <AnimatedHeader />
           <AnimatedMain>{children}</AnimatedMain>
