5525eef4a3f5bff9de745bf00319ef74e7ba0b3b
PBLGroupG <pblgroupg994@gmail.com>
2025-06-14 04:02:55 +0900

    diagramfix

--- app/SystemDesign/page.tsx
+++ app/SystemDesign/page.tsx
@@ -16,7 +16,7 @@ export default function SystemDesign() {
           </h2>
           <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
             <Image
-              src="/Users/enriqco/Documents/Github2.0/PBLGroupG.github.io/public/useCase.png"
+              src="/useCase.png"
               alt="Use Case Diagram"
               width={800}
               height={600}
@@ -32,7 +32,7 @@ export default function SystemDesign() {
           </h2>
           <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
             <Image
-              src="/Users/enriqco/Documents/Github2.0/PBLGroupG.github.io/public/classDiagram.png"
+              src="/classDiagram.png"
               alt="Sequence Diagram"
               width={800}
               height={600}
