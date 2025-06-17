1c3e517e6bf3a2c422a33065f39324d3ffe95dc7
PBLGroupG <pblgroupg994@gmail.com>
2025-06-17 14:25:40 +0900

    allDiagrams

--- app/SystemDesign/page.tsx
+++ app/SystemDesign/page.tsx
@@ -32,7 +32,38 @@ export default function SystemDesign() {
           </h2>
           <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
             <Image
-              src="/classdiagram.drawio.svg"
+              src="/classdiagram.drawio-2.svg"
+              alt="Class Diagram"
+              width={800}
+              height={600}
+              className="rounded-md"
+            />
+          </div>
+        </section>
+
+        <section className="mb-12">
+          <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-100">
+            3. State Diagram
+          </h2>
+          <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
+            <Image
+              src="/StateDiagram-3.svg"
+              alt="Sequence Diagram"
+              width={800}
+              height={600}
+              className="rounded-md"
+            />
+          </div>
+        </section>
+
+
+        <section className="mb-12">
+          <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-100">
+            4. System Overview Diagram
+          </h2>
+          <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
+            <Image
+              src="/SystemOverviewSVG.svg"
               alt="Sequence Diagram"
               width={800}
               height={600}
@@ -43,11 +74,26 @@ export default function SystemDesign() {
 
         <section className="mb-12">
           <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-100">
-            2. State Diagram
+            5. CrimeForecasting Activity Diagram
           </h2>
           <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
             <Image
-              src="/classDiagram.png"
+              src="/CrimeForecastingSVG.svg"
+              alt="Sequence Diagram"
+              width={800}
+              height={600}
+              className="rounded-md"
+            />
+          </div>
+        </section>
+
+        <section className="mb-12">
+          <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-100">
+            6. Sequence 1 
+          </h2>
+          <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
+            <Image
+              src="/Sequence1.drawio-2.svg"
               alt="Sequence Diagram"
               width={800}
               height={600}
@@ -59,11 +105,11 @@ export default function SystemDesign() {
 
         <section className="mb-12">
           <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-100">
-            2. Map Activity Diagram
+            7. Sequence 2
           </h2>
           <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
             <Image
-              src="/classDiagram.png"
+              src="/Sequence2.drawio-2.svg"
               alt="Sequence Diagram"
               width={800}
               height={600}
@@ -72,13 +118,14 @@ export default function SystemDesign() {
           </div>
         </section>
 
+
         <section className="mb-12">
           <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-100">
-            2. C
+            8 Sequence 3
           </h2>
           <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
             <Image
-              src="/StateDiagram.svg"
+              src="/Sequence3.drawio-2.svg"
               alt="Sequence Diagram"
               width={800}
               height={600}
