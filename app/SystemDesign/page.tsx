9ce008db2fd0ef880427e631d295d072feb41c93
PBLGroupG <pblgroupg994@gmail.com>
2025-06-17 13:02:32 +0900

    update

--- app/SystemDesign/page.tsx
+++ app/SystemDesign/page.tsx
@@ -41,6 +41,52 @@ export default function SystemDesign() {
           </div>
         </section>
 
+        <section className="mb-12">
+          <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-100">
+            2. State Diagram
+          </h2>
+          <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
+            <Image
+              src="/classDiagram.png"
+              alt="Sequence Diagram"
+              width={800}
+              height={600}
+              className="rounded-md"
+            />
+          </div>
+        </section>
+
+
+        <section className="mb-12">
+          <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-100">
+            2. Map Activity Diagram
+          </h2>
+          <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
+            <Image
+              src="/classDiagram.png"
+              alt="Sequence Diagram"
+              width={800}
+              height={600}
+              className="rounded-md"
+            />
+          </div>
+        </section>
+
+        <section className="mb-12">
+          <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-100">
+            2. C
+          </h2>
+          <div className="border border-gray-200 dark:border-gray-700 rounded-lg p-4 shadow-md bg-gray-50 dark:bg-gray-800">
+            <Image
+              src="/classDiagram.png"
+              alt="Sequence Diagram"
+              width={800}
+              height={600}
+              className="rounded-md"
+            />
+          </div>
+        </section>
+
         {/* Add more diagrams below as needed */}
       </div>
     </div>
